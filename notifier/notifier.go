ceedd9c41b2fd5bcdb5254cbc6403f735f8332b9
v.gorkij <v.gorkij@robo.cash>
2026-03-17 23:05:24 +0700

    TI-1: off message connection error

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -75,8 +75,8 @@ func (n *Notifier) NotifyConnectionError(err error) error {
 	if err == nil {
 		return errors.New("error is nil")
 	}
-	text := n.formatter.FormatConnectionError(err)
-	_, err = n.bot.SendMessage(text)
+	//_ := n.formatter.FormatConnectionError(err)
+	//_, err = n.bot.SendMessage(text)
 	return err
 }
 
